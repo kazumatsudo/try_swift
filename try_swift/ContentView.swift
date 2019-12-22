e16efde1e88405e501388009ea5430d7620ff753
kazumatsudo <kazuma.matsudo@icloud.com>
2019-12-22 16:58:25 +0900

    Initial Commit

---
+++ try_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  try_swift
+//
+//  Created by Kazuma Matsudo on 2019/12/22.
+//  Copyright © 2019 Kazuma Matsudo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ try_swiftTests/try_swiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  try_swiftTests.swift
+//  try_swiftTests
+//
+//  Created by Kazuma Matsudo on 2019/12/22.
+//  Copyright © 2019 Kazuma Matsudo. All rights reserved.
+//
+
+import XCTest
+@testable import try_swift
+
+class try_swiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
